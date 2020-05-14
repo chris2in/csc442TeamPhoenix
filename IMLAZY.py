eef3c804a18e9d8e13a36d82693fd58cfc7eb339
chris2in <38121755+chris2in@users.noreply.github.com>
2020-05-14 15:56:33 -0500

    Delete IMLAZY.py

--- IMLAZY.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-for i in range(10):
-    for o in range(10):
-        subprocess.call(['python3 STEG.py -r -b -o{} -i{} -w2.txt > {}S'.format(2**i,2**o,str(i*100+o)) ],shell=True)
-        
